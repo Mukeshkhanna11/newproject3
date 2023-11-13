a6d12765379d533d719827e9e617b90851524edd
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-13 16:18:23 +0530

    created newprject3

---
+++ src/Add.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Add = () => {
+  return (
+    <div>
+      <h1>hello im add component</h1>
+    </div>
+  );
+};
+
+export default Add;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const userInfo = [
+    { username: "rejesh", location: "Madurai" },
+    { username: "rejesh", location: "Madurai" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {userInfo.map((user) => (
+        <ul key={Math.random() * user.length}>
+          <li>{user.username}</li>
+          <li>{user.location}</li>
+        </ul>
+      ))}
+    </>
   );
 }
 
