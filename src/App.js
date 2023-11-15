ab4b2773797550bf916a601b857f4ee21eabb9d4
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-15 15:15:50 +0530

    updated file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import { FaCartArrowDown, FaGithub } from "react-icons/fa";
-import Cart from "./Cart";
+// import { FaCartArrowDown, FaGithub } from "react-icons/fa";
+// import Cart from "./Cart";
+import { useState } from "react";
 function App() {
+  const [username, setUsername] = useState("");
+  const handleChnage = (event) => {
+    setUsername(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`you typed:${username}`);
+    setUsername("");
+  };
   return (
     <>
-      <div>
+      {/* <div>
         <FaCartArrowDown />
       </div>
       <FaGithub />
-      <Cart />
+      <Cart /> */}
+      <h1>form demo</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={handleChnage} />
+        <button>submit</button>
+      </form>
     </>
   );
 }
